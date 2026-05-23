c65284b298fdf6cff11f0d62f4f41a1fa73429e4
Grazyna <g.daszyk@gmail.com>
2026-05-23 22:22:16 +0100

    secrets mngmt

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ env = dotenv_values(".env")
 ### Secrets using Streamlit  Cloud  Mechanism
 # https://docs. streamlit.io/deploy/streamlit-community-cloud/deploy-your-app/secrets-management
 if "QDRANT_URL" in st.secrets:
-    env["QDRANT_URL"]= st.secrets["QDRANT_URL"],
+    env["QDRANT_URL"]= st.secrets["QDRANT_URL"]
 if "QDRANT_API_KEY" in st.secrets:
     env["QDRANT_API_KEY"]=st.secrets["QDRANT_API_KEY"]
 ###
